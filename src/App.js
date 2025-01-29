25cdafe7869afd6ebee11a2d06e2841f864b5705
unknown <glimadefreitasgit2002@gmail.com>
2025-01-29 05:14:44 -0300

    Trabalhando com UseState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+
 import React from 'react'
-import Nome from './components/Nome';
+
+
 function App(){
+  const [aluno, setAluno] = useState('Sujeito programador')
+  
+  function handleChangeName(nome){
+    setAluno(nome);
+  }
   return(
     <div>
-      <h1>Componente App</h1>
-      <Nome aluno="Lucas" idade={30}/>
-      <br/>
-      <Nome aluno="Pedro" idade={25}/>
+      <h1>Componente App</h1><br/>
+      <h2>Olá {aluno}</h2>
+      <button onClick={() => handleChangeName('Guilherme Lima de Freitas')}>
+        Mudar nome
+      </button>
     </div>
   );
 }
